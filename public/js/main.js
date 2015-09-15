4130e33d77b6fbc1da4a7548d73de8ca62c24e0a
Brendan Luna <brendanluna1@gmail.com>
2015-09-15 16:22:36 -0700

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+    // input validation
+    $(document).ajaxStart(function() {
+        $("#loading").removeClass('hidden');
+        $('#searchIcon').hide();
+        $("#noResults").addClass('hidden');
+    }).ajaxStop(function() {
+        $("#loading").addClass('hidden');
+        $('#searchIcon').show();
+    });
+
+    var result = null;
+    var currentVideo = 0;
+    var angle1 = 0;
+    var angle2 = 0;
+
+    // onclick handler
+    $('#searchIcon').click(function() {
+        if ($('#searchBox').val() == '') {
+            alert('Search cannot be blank!');
+        } else {
+            currentVideo = 0;
+            $('#tvOff').css('display: none');
+            var parameters = {
+                search: $('#searchBox').val()
+            };
+
+            $.get('/search', parameters, function(data) {
+                console.log(data);
+                if (data === 'undefined') {
+                    $('#noResults').removeClass('hidden');
+                } else {
+                    result = data;
+                    var videoURL = "https://archive.org/embed/" + data.docs[currentVideo].identifier;
+                    $('#videoFrame').attr('src', videoURL);
+                    $('#videoTitle').html(data.docs[0].title);
+                    $('#videoDetails').html(data.docs[0].date + ' | Number of Downloads: ' + data.docs[0].downloads);
+                    $('#videoDescription').html(data.docs[0].description);
+                }
+            });
+        }
+    });
+
+    // on "enter" handler
+    $('#searchBox').keypress(function(e) {
+        if (e.which == 13) {
+            $('#searchIcon').click();
+        }
+    });
+
+    $('#hiddenButton1').click(function() {
+        angle1 += 45;
+        var $upperKnob = $('#UpperKnob');
+        TweenMax.to($upperKnob, .25, {
+            rotation: angle1,
+            transformOrigin: "50% 50%"
+        });
+
+        currentVideo = currentVideo + 1;
+        var videoURL = "https://archive.org/embed/" + result.docs[currentVideo].identifier;
+        $('#videoFrame').attr('src', videoURL);
+        $('#videoTitle').html(result.docs[currentVideo].title);
+        $('#videoDetails').html(result.docs[currentVideo].date + ' | Number of Downloads: ' + result.docs[currentVideo].downloads);
+        $('#videoDescription').html(result.docs[currentVideo].description);
+        $('#videoCollection').html(results.docs[currentVideo].collection[0]);
+    });
+
+    $("#hiddenButton2").on('click', function(e) {
+        angle2 += 45;
+        var $lowerKnob = $('#LowerKnob');
+        TweenMax.to($lowerKnob, .25, {
+            rotation: angle2,
+            transformOrigin: "50% 50%"
+        });
+
+    });
+
+});
---
+++ server.js
@@ -0,0 +1,44 @@
+// Get all packages needed
+var express = require('express');
+var app = express();
+var rest = require('restler');
+var request = require('request');
+
+// tell node where to look for site resources
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+
+	res.render('pages/index');
+});
+
+app.get('/search', function(req, res){
+	var val = encodeURIComponent(req.query.search); 
+	var url = "https://archive.org/advancedsearch.php?q=" + val +
+	"+AND+mediatype:movies+AND+collection:(home_movies+OR+prelingerhomemovies)" + 
+	"&fl[]=identifier,title,mediatype,collection,downloads,description,date,avg_rating&rows=15&output=json";
+	console.log(url);
+
+	request(url, function(err, resp, body){
+		body = JSON.parse(body);
+		console.log(body.response);
+		// logic used to compare search results with the input from user
+		 if (err){
+		 	console.log("error");
+		 } else if(body.response.numFound == 0){
+		 	console.log('"teststsetslajflkdjf');
+		 	res.send('undefined');
+		 } else {
+		 	// pass back the results to client side
+		 	res.send(body.response);
+		 }
+	});
+});
+
+// start server
+app.listen(8080);
+console.log("Server started!");
+
